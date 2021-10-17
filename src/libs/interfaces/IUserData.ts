c4b1d854dd078a74cc4f0452aa45277d5f366b4f
krung2 <jungbin4337@dgsw.hs.kr>
2021-10-17 19:56:56 +0900

    Create IUserData.ts

---
+++ src/libs/interfaces/IUserData.ts
@@ -0,0 +1,15 @@
+export interface ISchoolData {
+  idx: number;
+  name1: string;
+  name2: string;
+  github1: string;
+  github2: string;
+  position1: string;
+  position2: string;
+  intro: string;
+  contactType: string;
+  contact: string;
+  daeguTeam?: boolean;
+  gwangjuTeam?: boolean;
+  deadeokTeam?: boolean;
+};
