e9c7518f5e0345ad796fdd19d14540dcc6758a37
krung2 <jungbin4337@dgsw.hs.kr>
2021-10-17 20:00:02 +0900

    Update checkNullOrUndefind.ts

--- src/utils/checkNullOrUndefind.ts
+++ src/utils/checkNullOrUndefind.ts
@@ -1,5 +1,25 @@
+import { ISchoolData } from "../libs/interfaces/IUserData";
+
 export const checkNullOrUndefindArray = <T>(data: T) => {
 
   if (data === undefined || data === null) return [];
   return data;
+}
+
+export const checkNullOrUndefind = (data: string): boolean => {
+
+  if (data === undefined || data === null || data === '') return true;
+  return false;
+}
+
+export const checkNullCard = (cardData: ISchoolData) => {
+  if (checkNullOrUndefind(cardData.name1)) return false;
+  if (checkNullOrUndefind(cardData.name2)) return false;
+  if (checkNullOrUndefind(cardData.github1)) return false;
+  if (checkNullOrUndefind(cardData.github2)) return false;
+  if (checkNullOrUndefind(cardData.position1)) return false;
+  if (checkNullOrUndefind(cardData.position2)) return false;
+  if (checkNullOrUndefind(cardData.intro)) return false;
+  if (checkNullOrUndefind(cardData.contact)) return false;
+  return true;
 }
